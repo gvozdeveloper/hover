b35304246a38cafd24d824510711e029d3e09731
Alexey Gvozdev <79114367+gvozdeveloper@users.noreply.github.com>
2021-12-15 22:13:20 +0300

    создал файл js

---
+++ app.js
@@ -0,0 +1,32 @@
+const board = document.querySelector('#board')
+const colors = ['#cf4c4c', '#64da16', '#3b53db', '#d80d83', '#bb83a4', '#e4e70b']
+const SQUARES_NUMBER = 500
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => 
+    setColor(square))
+
+    square.addEventListener('mouseleave', () => 
+    removeColor(square))
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
